8b689e317032be871dac73f8d31cfac37da457b1
balu0107 <createbalu@gmail.com>
2026-09-02 20:48:59 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -42,8 +42,8 @@ resource "aws_route_table_association" "public" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block  = "0.0.0.0/0"
-    instance_id = aws_instance.nat_instance.id
+    cidr_block           = "0.0.0.0/0"
+    network_interface_id = aws_instance.nat_instance.primary_network_interface_id
   }
   tags = { Name = "corp-private-rt" }
 }
