dbb74e5205e995a1de9617e69ba51e8db6615d2f
balu0107 <createbalu@gmail.com>
2026-09-08 02:24:03 +0530

    upated compute

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,8 @@ variable "key_name" {
   type    = string
   default = "devops-key"
 }
+
+variable "ssh_public_key" {
+  description = "The public SSH key to inject into instances"
+  type        = string
+}
