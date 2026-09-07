fe9807a85012c674a5eb05ff5cdb868378ac4520
balu0107 <createbalu@gmail.com>
2026-09-08 00:53:29 +0530

    Fix compute

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -75,7 +75,7 @@ resource "aws_instance" "k8s_master" {
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.k8s_cluster_sg.id]
   associate_public_ip_address = true
-  key_name                    = var.key_name
+  key_name                    = "devops-key"
 
   tags = {
     Name = "k8s-master"
@@ -88,7 +88,7 @@ resource "aws_instance" "k8s_worker_1" {
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.k8s_cluster_sg.id]
   associate_public_ip_address = true
-  key_name                    = var.key_name
+  key_name                    = "devops-key"
 
   tags = {
     Name = "k8s-worker-1"
@@ -101,7 +101,7 @@ resource "aws_instance" "k8s_worker_2" {
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.k8s_cluster_sg.id]
   associate_public_ip_address = true
-  key_name                    = var.key_name
+  key_name                    = "devops-key"
 
   tags = {
     Name = "k8s-worker-2"
