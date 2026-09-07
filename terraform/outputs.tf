ade9dae001c420a612b98bdb2be8fc5ca01d7aeb
balu0107 <createbalu@gmail.com>
2026-09-08 00:39:31 +0530

    Fix output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,8 +7,3 @@ output "worker_public_ips" {
   value       = [aws_instance.k8s_worker_1.public_ip, aws_instance.k8s_worker_2.public_ip]
   description = "Public IP addresses of the Kubernetes worker nodes"
 }
-
-output "private_key_pem" {
-  value     = tls_private_key.k8s_key.private_key_pem
-  sensitive = true
-}
